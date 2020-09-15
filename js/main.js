04de65d6dbad367c8ee624667c8bed8aa317a83b
stasvol <https://github.com/stasvol/psychologist-online.git>
2020-09-16 01:10:19 +0300

    updated files

--- js/main.js
+++ js/main.js
@@ -28,13 +28,46 @@
        dots[slideIndex-1].className += " active";
    }
 
-       // TAB
+       // __________TAB______________
 
-   function openImg(imgName) {
-       var i, x;
-       x = document.getElementsByClassName("picture");
-       for (i = 0; i < x.length; i++) {
-           x[i].style.display = "none";
-       }
-       document.getElementById(imgName).style.display = "block";
+
+
+
+   const tabsBtn   = document.querySelectorAll(".tabs__nav-btn");
+   const tabsItems = document.querySelectorAll(".tabs__item");
+
+   tabsBtn.forEach(onTabClick);
+
+   function onTabClick(item) {
+       item.addEventListener("click", function() {
+           let currentBtn = item;
+           let tabId = currentBtn.getAttribute("data-tab");
+           let currentTab = document.querySelector(tabId);
+
+           if( ! currentBtn.classList.contains('active') ) {
+               tabsBtn.forEach(function(item) {
+                   item.classList.remove('active');
+               });
+
+               tabsItems.forEach(function(item) {
+                   item.classList.remove('active');
+               });
+
+               currentBtn.classList.add('active');
+               currentTab.classList.add('active');
+           }
+       });
    }
+
+   // document.querySelector('.tabs__nav-btn').click();
+
+
+
+   // function openImg(imgName) {
+   //     var i, x;
+   //     x = document.getElementsByClassName("picture");
+   //     for (i = 0; i < x.length; i++) {
+   //         x[i].style.display = "none";
+   //     }
+   //     document.getElementById(imgName).style.display = "block";
+   // }
