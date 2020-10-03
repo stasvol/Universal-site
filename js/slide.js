bbf0ab19c8971af2e8960494ebbc5fbf0c90f9a5
stasvol <https://github.com/stasvol/psychologist-online.git>
2020-10-04 01:15:35 +0300

    updated files

--- js/slide.js
+++ js/slide.js
@@ -34,3 +34,79 @@ function showGallery(n) {
     // captionText.innerHTML = dot[counter-1].alt;
 }
 
+var elSlide = document.querySelectorAll ('.bg_slide1');
+var sectionBackground = document.getElementById('bg_section');
+
+
+console.log(elSlide);
+elSlide.forEach(function (el) {
+    el.addEventListener('click', function () {
+
+        // const attr = elSlide.getAttribute('src');
+        // sectionBackground.style.backgroundImage = `url('${attr}')`;
+        sectionBackground.style.backgroundImage = "url('" + el.getAttribute('src') + "')";
+
+
+    } );
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.body.style.backgroundImage = "url('img_tree.png')";
+//
+// function bg() {
+//
+//     var imgCount = 3;
+//     var dir = 'http://local.statamic.com/_themes/img/';
+//     // I changed your random generator        Я поменял твой генератор случайных
+//     //floor: helps getting a random integer   этаж: помогает получить случайное целое число
+//     var randomCount = (Math.floor(Math.random() * imgCount));
+//     // I changed your array to the literal notation. The literal notation is preferred. Я изменил ваш массив на буквальное обозначение. Буквальное обозначение является предпочтительным.
+//     var images = ['001.png', '002.png', '003.png'];
+//     // I changed this section to just define the style attribute the best way I know how.  Я изменил этот раздел, чтобы просто определить атрибут стиля как можно лучше.
+//     document.getElementById('banner').setAttribute("style", "background-image: url(" + dir + images[randomCount] + ");background-repeat: no-repeat;background-size: 388px 388px");
+// }
+// // Don't forget to run the function instead of just defining it.   Не забудьте запустить функцию, а не просто определить ее.
+// bg();
